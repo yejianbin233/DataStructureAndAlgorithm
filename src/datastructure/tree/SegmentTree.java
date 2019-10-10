89b7cfaeef84b19b20c7d67356633897a6e0f80f
@yejianbin233 <877811992@qq.com>
2019-10-10 21:30:50 +0800

    complete

--- src/datastructure/tree/SegmentTree.java
+++ src/datastructure/tree/SegmentTree.java
@@ -10,9 +10,13 @@ package datastructure.tree;/**
  * @Date 2019/10/8 21:39
  **/
 public class SegmentTree<E> {
+    // 构建的线段树
     private E[] tree;
+    // 传入的原始数据
     private E[] data;
+    // 根据不同的 Merger 实例，计算不同的合并结果
     private Merger<E> merger;
+
     public SegmentTree(E[] arr,Merger<E> merger){
         data = (E[]) new Object[arr.length];
         for (int i=0;i<data.length;i++) {
@@ -20,8 +24,11 @@ public class SegmentTree<E> {
         }
 
         // 线段树在极端情况下，需要原始数组的 4 倍容量才能存放所有元素
+        //TODO 为什么要 4 倍？两倍不行吗？线段树构造时的深度如何延伸的？
         tree = (E[]) new Object[4 * arr.length];
+
         this.merger = merger;
+        // 构建线段树
         buildSegmentTree(0,0,data.length-1);
     }
 
@@ -31,21 +38,23 @@ public class SegmentTree<E> {
     *
     * 如 [1,2,3,4,5,6] 数组，想获取 2~4 区间内的元素总和，构建的线段树如下：
     *
-    *                       21
-    *                      /  \
-     *                    6    15
-     *                   / \   / \
-      *                 1   5 4   11
-        *                  / \    / \
-    *                     2   3  5   6
+    *                        21
+    *                      /    \
+     *                    6      15
+     *                   / \     / \
+      *                 1   5   4   11
+        *                  / \     / \
+    *                     2   3   5   6
+    *
+    *                   ps：所有的叶子节点组成了原始的 data 数组.
     * 思路：
     *
     * 1. 使用递归
     *
-    * 2.
+    * 2. 通过当前节点的左右子节点的值来得出当前节点的目标值
     * */
     private void buildSegmentTree(int treeIndex,int l,int r){
-        // 递归基
+        // 递归基，当区间缩小为 1 时，就可以直接该区间内的区域目标值
         if (l == r) {
             // 注意 data[l] = data[r]
             tree[treeIndex] = data[l];
@@ -55,13 +64,15 @@ public class SegmentTree<E> {
         int mid = l + (r - l)/2;
         int leftChild = leftChild(treeIndex);
         int rightChild = rightChild(treeIndex);
-        // 自底向上构建，
+        // 自底向上构建
         buildSegmentTree(leftChild,l,mid);
         buildSegmentTree(rightChild,mid+1,r);
 
+        // 计算当前节点的目标值
         // 根据业务的不同来定义不同的逻辑来获取区间的数据，示例是获取两个参数（子节点的值）的和来组成父节点的值
         tree[treeIndex] = merger.merge(tree[leftChild],tree[rightChild]);
     }
+
     public int getSize(){
         return data.length;
     }
@@ -72,25 +83,70 @@ public class SegmentTree<E> {
         }
         return query(0,0,data.length,queryL,queryR);
     }
+    /*
+    * 查询区间的目标值有三种情况：
+    *
+    * 当目标区间全在一边（1. 左边 / 2.右边）
+    *
+    * 3. 当目标区间同时存在左右两边
+    * */
     private E query(int treeIndex,int l,int r,int queryL,int queryR){
-        // 递归基
+        // 递归基，当查找的区间与节点的区间相匹配
         if (l == queryL && r == queryR) {
             // 当查找的区间匹配时，tree[treeIndex] 就是目标值
             return tree[treeIndex];
         }
         int mid = l + (r-l) / 2;
+
         if (queryL >= mid + 1) {
+            // 目标区间全在右半区间
             return query(rightChild(treeIndex),mid+1,r,queryL,queryR);
         } else if (queryR <= mid) {
+            // 目标区间全在左半区间
             return query(leftChild(treeIndex),l,mid,queryL,queryR);
         }
 
+        // 目标区间同时在左右区间内
         E leftResult = query(leftChild(treeIndex),l,mid,queryL,mid);
         E rightResult = query(rightChild(treeIndex),mid+1,r,mid+1,r);
 
+        // 合并结果
         return merger.merge(leftResult,rightResult);
     }
 
+    public void set(int index, E val){
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("index is illegal.");
+        }
+        //TODO 为什么是 data.length-1 而不是 tree.length - 1???
+        set(0,0,data.length-1,index,val);
+    }
+    /*
+    * 更新线段树中的某个节点的值与构建线段树非常的思路类似
+    *
+    * 在线段树的区间中不断的向下寻找的 l = r 的节点，该节点就是需要更新的值。
+    *
+    * 然后在从该节点自底向上维护线段树中经过的父节点的值即可。
+    * */
+    private void set(int treeIndex, int l, int r, int targetIndex, E val){
+        if ( l == r) {
+            tree[treeIndex] = val;
+            return;
+        }
+
+        int mid = l + (r-l)/2;
+        int leftChild = leftChild(treeIndex);
+        int rightChild = rightChild(treeIndex);
+
+        if (targetIndex >= mid + 1) {
+            set(rightChild,mid + 1,r,targetIndex,val);
+        } else {
+            set(leftChild,l,mid,targetIndex,val);
+        }
+
+        // 维护父节点的值
+        tree[treeIndex] = merger.merge(tree[leftChild],tree[rightChild]);
+    }
     public E get(int index){
         if (index < 0 || index >= data.length) {
             throw new IllegalArgumentException("index is illegal");
@@ -111,6 +167,8 @@ public class SegmentTree<E> {
         Integer[] arr = {1,2,4,5,6,7,8,9,0};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(arr,new IntegerMerger());
 
+        System.out.println(segmentTree.query(2,4));
+        segmentTree.set(2,3);
         System.out.println(segmentTree.query(2,4));
     }
 }
