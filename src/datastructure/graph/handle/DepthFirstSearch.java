e6fe42fd544ec9128583b4322b0ec337b0583d2a
@yejianbin233 <877811992@qq.com>
2019-10-23 10:39:12 +0800

    graph handle class

--- src/datastructure/graph/handle/DepthFirstSearch.java
+++ src/datastructure/graph/handle/DepthFirstSearch.java
@@ -2,12 +2,58 @@ package datastructure.graph.handle;/**
  * Created by Administrator on 2019/10/22.
  */
 
+import datastructure.graph.Graph;
+
+import java.util.Iterator;
+
 /**
  * @author yejianbin
  * @version 1.0
  * @ClassName DepthFirstSearch
- * @Description TODO
+ * @Description 深度优先遍历 - 遍历所有的边
+ *
+ * 类似走迷宫思路｛
+ *
+ *   1. 选择一条没有标记过的通道，在通道上铺一条绳子；
+ *   2. 标记所有第一次路过的路口和通道；
+ *   3. 当来到一个标记过的路口时，回退到上一个路口；
+ *   4. 当回退到的路口没有可走的通道时继续回退。
+ *
+ * ｝
  * @Date 2019/10/22 11:12
  **/
 public class DepthFirstSearch {
+    // 标记通过的路口（顶点）
+    private boolean[] marked;
+    private int count;
+
+    public DepthFirstSearch(Graph g, int s) {
+        // 每个顶点对应一个 marked 下标
+        marked = new boolean[g.V()];
+        dfs(g, s);
+    }
+
+    private void dfs(Graph g, int v){
+        // 标记当前顶点已经被走过
+        this.marked[v] = true;
+        this.count++;
+        // 当前顶点的边的迭代器
+        Iterator<Integer> it = g.adj(v);
+        // while 循环隐含着回退，当循环结束回退到上一个路口（顶点）
+        while(it.hasNext()){
+            // 如果 v 连接的顶点 w 没有被走过，那么先以该顶点 w 深入遍历（深度优先遍历）
+            int w = it.next();
+            if (!marked[w]) {
+                // 使用递归，进入下一个路口继续遍历
+                dfs(g,w);
+            }
+        }
+    }
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public int count() {
+        return this.count;
+    }
 }
--- src/datastructure/graph/handle/Search.java
+++ src/datastructure/graph/handle/Search.java
@@ -2,12 +2,108 @@ package datastructure.graph.handle;/**
  * Created by Administrator on 2019/10/22.
  */
 
+import datastructure.graph.Graph;
+
+import java.util.Iterator;
+
 /**
  * @author yejianbin
  * @version 1.0
  * @ClassName Search
- * @Description TODO
+ * @Description 图处理算法 API - Search
  * @Date 2019/10/22 10:32
  **/
 public class Search {
+
+    private int s;
+    private Graph g;
+    private int count;
+
+    // 找到和起点 s 连通的所有顶点
+    public Search(Graph g, int s) {
+        this.s = s;
+        this.g = g;
+        count = 0;
+    }
+
+    // 判断 v 和 s 是否连通
+    boolean marked(int v) {
+        Iterator<Integer> it = g.adj(s);
+        while (it.hasNext()){
+            if (it.next() == v){
+                count++;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 与 s 连通的顶点总数
+    int count() {
+        return this.count;
+    }
+
+    // 测试运行
+    public static void main(String[] args) {
+        // 获取初始化图, true - 连通图 : false - 非连通图
+//        Graph g = initGraph(true);
+        Graph g = initGraph(false);
+        int s = 1;
+        Search search = new Search(g, s);
+
+        for (int v = 0; v < g.V(); v++) {
+            if (search.marked(v)) {
+                System.out.println(v + " ");
+            }
+            System.out.println();
+        }
+
+        // 如果顶点 s 的连接顶点数与图中的顶点数与边的对应关系 g.V() - 1 不一样则说明不是连通图
+        if (search.count() != g.V() - 1) {
+            System.out.println("Not ");
+        } else {
+            System.out.println("connected");
+        }
+    }
+
+    public static Graph initGraph(boolean isConnected){
+        return isConnected ? connectedGraph() : notGraph();
+    }
+    private static Graph notGraph(){
+        Graph g = new Graph(7);
+        // 0 - {1,2,5,6}
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(0,5);
+        g.addEdge(0,6);
+        // 1 - {0}
+        g.addEdge(1,0);
+        // 2 - {0}
+        g.addEdge(2,0);
+        // 5 - {0,3,4}
+        g.addEdge(5,0);
+        g.addEdge(5,3);
+        g.addEdge(5,4);
+        // 3 - {5,4}
+        g.addEdge(3,5);
+        g.addEdge(3,4);
+        // 4 - {3,5,6}
+        g.addEdge(4,3);
+        g.addEdge(4,4);
+        g.addEdge(4,6);
+        // 6 - {0,4}
+        g.addEdge(6,0);
+        g.addEdge(6,4);
+        return g;
+    }
+    private static Graph connectedGraph(){
+        Graph g = new Graph(2);
+
+        // 0 - {1}
+        g.addEdge(0,1);
+        // 1 - {0}
+        g.addEdge(1,0);
+
+        return g;
+    }
 }
